adb7b5f18d8e5fbb2f22c5015ab7738d9dbb0dc7
Naman Muley <naman.g.muley@gmail.com>
2025-07-31 09:55:57 +0530

    Add app/database.py

---
+++ app/database.py
@@ -0,0 +1,16 @@
+import os
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker
+
+DATABASE_URL = os.getenv('DATABASE_URL', 'postgresql+asyncpg://postgres:postgres@db:5432/postgres')
+
+engine = create_async_engine(DATABASE_URL, echo=True, future=True)
+AsyncSessionLocal = sessionmaker(
+    bind=engine,
+    class_=AsyncSession,
+    expire_on_commit=False
+)
+
+async def get_async_session():
+    async with AsyncSessionLocal() as session:
+        yield session
